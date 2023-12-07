9a7f276de5b846c2989e04bd32fadf40192e4477
vonjo <vonjo@github.com>
2023-12-07 01:16:54 -1000

    add ip input validation

--- geoip-simple.py
+++ geoip-simple.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
+import ipaddress
 
 def validate_authentication():
     load_dotenv()
@@ -18,9 +19,22 @@ def validate_authentication():
     access_key = os.getenv('IPSTACK_ACCESS_KEY')
     return access_key
 
+def validate_ip_addr():
+    # check for argv input
+    if len(sys.argv) < 2:
+        print('\nError: Missing an ipv4 or ipv6 address to check!\n')
+        exit()
+    ip_addr = str(sys.argv[1]) # only read the first arg.
+    # perform input validation on the ipv4 or ipv6 address.
+    if not ipaddress.ip_address(ip_addr) or ipaddress.ip_address(ip_addr).is_private:
+        print('\nError: ' + ip_addr + ' is not a valid public ip address!\n')
+        exit()
+    return ip_addr
 
 def main():
     access_key = validate_authentication()
-    print(access_key)
+    #print(access_key)
+    ip_addr = validate_ip_addr()
+    print(ip_addr)
 
 main()
